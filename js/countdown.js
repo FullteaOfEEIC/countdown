f606902339bde967faf2ccda4a832b5bda1de90d
FullteaOfEEIC <rikuta0811@g.ecc.u-tokyo.ac.jp>
2019-05-30 22:13:48 +0900

    fix: tiny things

--- js/countdown.js
+++ js/countdown.js
@@ -1,78 +1,86 @@
-window.onload=main
+window.onload = main
 
-var speed=0.1;
+var speed = 0.1;
 
-function timeshow(data){
+function timeshow(data) {
     var now = new Date();
 
     //parameters
     var year = now.getFullYear();
-    var month = now.getMonth() + 1;//month of JavaScript is 0 to 11;
-    var day = now.getDay();
-    var hour=data.hour;
-    var minute=data.minute;
+    var month = now.getMonth() + 1; //month of JavaScript is 0 to 11;
+    var day = now.getDate();
+    var hour = data.hour;
+    var minute = data.minute;
 
 
     var second = 0;
 
-
-    var limit = new Date(year,month-1,day,hour,minute,second);
-    if(limit<0){
+    var limit = new Date(year, month - 1, day, hour, minute, second);
+    var left = limit - now;
+    if (left < 0) {
         startAnimation();
+        return 1;
     }
 
 
-    var left = limit - now;
-    cnt_day = Math.floor(left/(1000*60*60*24));
-    left = left -(cnt_day*(1000*60*60*24));
-    cnt_hour = Math.floor(left/(1000*60*60));
-    left = left -(cnt_hour*(1000*60*60));
-    cnt_min = Math.floor(left/(1000*60));
-    left = left -(cnt_min*(1000*60));
-    cnt_sec = Math.floor(left/1000);
-    left = left -(cnt_sec*(1000));
-    cnt_millisec = Math.floor(left/10);
+    cnt_day = Math.floor(left / (1000 * 60 * 60 * 24));
+    left = left - (cnt_day * (1000 * 60 * 60 * 24));
+    cnt_hour = Math.floor(left / (1000 * 60 * 60));
+    left = left - (cnt_hour * (1000 * 60 * 60));
+    cnt_min = Math.floor(left / (1000 * 60));
+    left = left - (cnt_min * (1000 * 60));
+    cnt_sec = Math.floor(left / 1000);
+    left = left - (cnt_sec * (1000));
+    cnt_millisec = Math.floor(left / 10);
 
 
 
-    document.getElementById("hour").getElementsByTagName("p")[0].innerHTML=Math.floor(cnt_hour%10);
-    document.getElementById("minute10").getElementsByTagName("p")[0].innerHTML=Math.floor(cnt_min/10);
-    document.getElementById("minute1").getElementsByTagName("p")[0].innerHTML=cnt_min%10;
-    document.getElementById("second10").getElementsByTagName("p")[0].innerHTML=Math.floor(cnt_sec/10);
-    document.getElementById("second1").getElementsByTagName("p")[0].innerHTML=cnt_sec%10;
+    document.getElementById("hour").getElementsByTagName("p")[0].innerHTML = Math.floor(cnt_hour % 10);
+    document.getElementById("minute10").getElementsByTagName("p")[0].innerHTML = Math.floor(cnt_min / 10);
+    document.getElementById("minute1").getElementsByTagName("p")[0].innerHTML = cnt_min % 10;
+    document.getElementById("second10").getElementsByTagName("p")[0].innerHTML = Math.floor(cnt_sec / 10);
+    document.getElementById("second1").getElementsByTagName("p")[0].innerHTML = cnt_sec % 10;
 
-    setTimeout(`$.getJSON("./data.json", timeshow)`,100);
+    setTimeout(`$.getJSON("./data.json", timeshow)`, 100);
 
 
 }
 
-function scroll(timestamp){
-    const screenWidth=window.parent.screen.width;
-    const target=document.getElementById("announce");
-    var objectWidth=target.scrollWidth;
-    var X = (-0.1*timestamp)%(screenWidth+objectWidth)+screenWidth;
+function scroll(timestamp) {
+    const screenWidth = window.parent.screen.width;
+    const target = document.getElementById("announce");
+    var objectWidth = target.scrollWidth;
+    var X = (-0.1 * timestamp) % (screenWidth + objectWidth) + screenWidth;
     target.style.transform = `translate(${X}px)`;
     requestAnimationFrame(scroll)
 
 }
 
-function message(data){
+function message(data) {
     $("#announce").text("");
-    for(var i in data.text[0]){
-        $("#announce").append('<div class="text"><p>'+data.text[0][i]+'</p></div>')
+    for (var i in data.text[0]) {
+        $("#announce").append('<div class="text"><p>' + data.text[0][i] + '</p></div>')
     }
-    setTimeout(`$.getJSON("./data.json", message)`,10000);
+    setTimeout(`$.getJSON("./data.json", message)`, 10000);
 
 }
 
-function main(){
+function main() {
     $.getJSON("./data.json", timeshow);
     $.getJSON("./data.json", message);
     requestAnimationFrame(scroll);
 };
 
 
-function startAnimation(){
-    console.log("finished");
-    $("body").text("");
+function startAnimation() {
+    console.log("Animation Start");
+    fade(0);
+}
+
+function fade(opacity) {
+    document.getElementById("animation").style.opacity = opacity;
+    if (opacity > 1) {
+        return 0;
+    }
+    setTimeout(`fade(` + (opacity + 0.01) + `)`, 10)
 }
